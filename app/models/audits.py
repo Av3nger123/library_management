f8f2cfaa8af5b14bf79793f6f78067da640dd6eb
Anyam Sai Praneeth Reddy <anyam.reddy@1silverbullet.tech>
2025-01-26 14:44:51 +0530

    return book validations

--- app/models/audits.py
+++ app/models/audits.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, root_validator, validator
 
 class AuditBase(BaseModel):
     user_id: int
-    book_id:int
-    status:str = "assigned"
+    book_id: int
+    status: str = "assigned"
 
-    
 class Audit(AuditBase):
     id: int
     book_item_id: int  
@@ -22,8 +21,21 @@ class AssignBook(AuditBase):
     pass
 
 class ReturnBook(BaseModel):
-    id:int
+    id: int
+    status: str = "returned"
     condition: str = "good"
+    
+    @root_validator(pre=True)
+    def check_status_and_condition(cls, values):
+        status = values.get('status')
+        condition = values.get('condition')
+        if status == "lost":
+            values['condition'] = None
+        elif status == 'returned' and condition not in ['good','bad']:
+            raise ValueError("Condition can only be 'good' or 'bad'")
+        elif status and status not in ['returned','lost'] :
+            raise ValueError("Status can only be 'returned' or 'lost'")
+        return values
 
 
 
